4dc0f122ab7059fc631a9c7702fcef9ef8f26f25
Админ <@DESKTOP-8DFR7S5>
2022-10-27 01:58:38 +0300

    add ViewModel class

---
+++ 1Password/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using data_base.Entities;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1Password
+{
+    [AddINotifyPropertyChangedInterface]
+    public class ViewModel
+    {
+        private ObservableCollection<AccountInfo> accounts;
+
+        public ViewModel()
+        {
+            accounts = new ObservableCollection<AccountInfo>();
+        }
+        
+        public IEnumerable<AccountInfo> AccountsInfo => accounts;
+        public void AddMessage(AccountInfo account)
+        {
+            accounts.Add(account);
+        }
+        public void ClearMessages()
+        {
+            accounts.Clear();
+        }
+    }
+
+    [AddINotifyPropertyChangedInterface]
+    public class AccountInfo
+    {
+        
+    }
+}
