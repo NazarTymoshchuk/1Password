2e21827969b29e7fe78eb86f581954a62234dd2e
Nazar <Timo_qm01@student.itstep.org>
2022-10-30 23:06:50 +0200

    Create Hash password

--- 1Password/Registration.xaml.cs
+++ 1Password/Registration.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +21,10 @@ namespace _1Password
     /// </summary>
     public partial class Registration : Window
     {
+        private const int SaltSize = 16;
+
+        private const int HashSize = 20;
+
         OnePasswordDbContext context;
         public Registration()
         {
@@ -34,7 +39,9 @@ namespace _1Password
             {
                 if (txtPassword.Password == txtComfirmPassword.Password)
                 {
-                    MainWindow main = new MainWindow(txtUsername.Text, txtPassword.Password, false);
+                    var hash = Hash(txtPassword.Password);
+
+                    MainWindow main = new MainWindow(txtUsername.Text, hash, false);
                     main.Show();
                     this.Close();
                 }
@@ -49,6 +56,33 @@ namespace _1Password
             {
                 txtUsernameTaken.Text = "Username is already taken";
             }          
+        }
+
+        public static string Hash(string password, int iterations)
+        {
+            // Create salt
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[SaltSize]);
+
+            // Create hash
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            var hash = pbkdf2.GetBytes(HashSize);
+
+            // Combine salt and hash
+            var hashBytes = new byte[SaltSize + HashSize];
+            Array.Copy(salt, 0, hashBytes, 0, SaltSize);
+            Array.Copy(hash, 0, hashBytes, SaltSize, HashSize);
+
+            // Convert to base64
+            var base64Hash = Convert.ToBase64String(hashBytes);
+
+            // Format hash with extra information
+            return string.Format("$MYHASH$V1${0}${1}", iterations, base64Hash);
+        }
+
+        public static string Hash(string password)
+        {
+            return Hash(password, 10000);
         }
     }
 }
